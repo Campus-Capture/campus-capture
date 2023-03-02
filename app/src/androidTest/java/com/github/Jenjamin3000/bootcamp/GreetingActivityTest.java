f0f6aeb8cf6a5254633e6714606c4e0a86850c6b
Benjamin Beuret <benjamin.beuret@epfl.ch>
2023-03-02 17:24:50 +0100

    Fix of a GreetingActivityTest.java test

--- app/src/androidTest/java/com/github/Jenjamin3000/bootcamp/GreetingActivityTest.java
+++ app/src/androidTest/java/com/github/Jenjamin3000/bootcamp/GreetingActivityTest.java
@@ -1,6 +1,7 @@
 package com.github.Jenjamin3000.bootcamp;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
 
 import androidx.test.core.app.ActivityScenario;
@@ -8,11 +9,13 @@ import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import  static androidx.test.espresso.Espresso.onView;
+import static org.junit.Assert.assertEquals;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -23,4 +26,10 @@ public class GreetingActivityTest {
         ActivityScenario.launch(myIntent).close();
         onView(ViewMatchers.withId(R.id.greetingText)).noActivity();
     }*/
+    @Test
+    public void SimpleTest(){
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.github.Jenjamin3000.bootcamp", appContext.getPackageName());
+    }
 }
