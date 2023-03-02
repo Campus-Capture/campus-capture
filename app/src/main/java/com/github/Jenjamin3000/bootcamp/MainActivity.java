491af97c8726beda013c399081384658c1f7766f
06Elija <elija.dirren@gmail.com>
2023-03-02 18:59:30 +0100

    Added a custom tool bar to the top of the screen, with a menu button (on the wrong side)

--- app/src/main/java/com/github/Jenjamin3000/bootcamp/MainActivity.java
+++ app/src/main/java/com/github/Jenjamin3000/bootcamp/MainActivity.java
@@ -2,11 +2,15 @@ package com.github.Jenjamin3000.bootcamp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create the toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         Button mainGoButton = findViewById(R.id.mainGoButton);
         TextView userNameText = findViewById(R.id.mainUserNameText);
@@ -30,5 +38,30 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(myIntent);
             }
         });
+    }
+
+    /**
+     * Create the options menu when initializing the toolbar
+     * @param menu The options menu in which you place your items.
+     *
+     * @return success
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the toolbar to add the items to the tool bar if they are present
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.action_navigate:
+                // TODO: open the navigation screen
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
