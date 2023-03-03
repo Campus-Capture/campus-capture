dc413cd735ba28225514585b3d3711111a5443fb
Mopplikus <elija-angelo.dirren@epfl.ch>
2023-03-03 14:38:01 +0100

    Fixed the tests

--- app/src/androidTest/java/com/github/Jenjamin3000/bootcamp/MainActivityTest.java
+++ app/src/androidTest/java/com/github/Jenjamin3000/bootcamp/MainActivityTest.java
@@ -2,6 +2,9 @@ package com.github.Jenjamin3000.bootcamp;
 
 import static androidx.test.espresso.Espresso.onView;
 
+import android.view.View;
+
+import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.intent.Intents;
@@ -9,6 +12,7 @@ import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.Matchers;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,83 +23,73 @@ public class MainActivityTest {
     @Rule
     public ActivityScenarioRule<MainActivity> testRule = new ActivityScenarioRule<>(MainActivity.class);
 
-    // --- Test stubs: horrible implementations because the setup wouldn't run on my machine
-    // Absolutely fix / clean this up before deploying
-
-    @Ignore("Test on other machine")
     @Test
     public void NavigationDrawerOpensWhenMenuButtonPressed()
     {
         Intents.init();
-        onView(ViewMatchers.withId(R.drawable.menu_icon)).perform(ViewActions.click());
+        onView(ViewMatchers.withContentDescription("Navigate up"))
+                .perform(ViewActions.click());
 
-        onView(ViewMatchers.withId(R.id.nav_view)).check(ViewAssertions.matches(ViewMatchers.isClickable()));
+        onView(ViewMatchers.withId(R.id.nav_view))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
         Intents.release();
     }
 
-    @Ignore("Test on other machine")
     @Test
     public void NavigationDrawerClosesWhenActionIsExecuted()
     {
         Intents.init();
-        onView(ViewMatchers.withId(R.drawable.menu_icon)).perform(ViewActions.click());
+        onView(ViewMatchers.withContentDescription("Navigate up"))
+                .perform(ViewActions.click());
+
         onView(ViewMatchers.withId(R.id.nav_main)).perform(ViewActions.click());
 
-        onView(ViewMatchers.withId(R.id.nav_view)).check(ViewAssertions.matches(ViewMatchers.isNotClickable()));
+        onView(ViewMatchers.withId(R.id.nav_view))
+                .check(ViewAssertions.matches(Matchers.not(ViewMatchers.isDisplayed())));
+
         Intents.release();
     }
 
-    @Ignore("Test on other machine")
     @Test
-    public void NavigationDrawerClosesWhenClickingOutsideOfDrawer()
-    {
+    public void NavigationDrawerClosesWhenSwiped() {
         Intents.init();
-        onView(ViewMatchers.withId(R.drawable.menu_icon)).perform(ViewActions.click());
-        onView(ViewMatchers.withId(R.id.app_bar_navigation_drawer)).perform(ViewActions.click());
+        onView(ViewMatchers.withContentDescription("Navigate up"))
+                .perform(ViewActions.click());
 
-        onView(ViewMatchers.withId(R.id.nav_view)).check(ViewAssertions.matches(ViewMatchers.isNotClickable()));
+        onView(ViewMatchers.withId(R.id.nav_view))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
+        onView(ViewMatchers.withId(R.id.nav_view)).perform(ViewActions.swipeLeft());
+
+        onView(ViewMatchers.withId(R.id.nav_view))
+                .check(ViewAssertions.matches(Matchers.not(ViewMatchers.isDisplayed())));
+
         Intents.release();
     }
 
-    @Ignore("Test on other machine")
     @Test
-    public void MainActivityFragmentContainerIsUpdatedWhenActionIsExecuted()
-    {
+    public void MainActivityFragmentContainerIsUpdatedWhenActionIsExecuted() {
         Intents.init();
-        onView(ViewMatchers.withId(R.id.fragmentContainerViewMain)).check(ViewAssertions.matches(ViewMatchers.withText("Main fragment")));
+        onView(ViewMatchers.withId(R.id.textMain))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
 
-        onView(ViewMatchers.withId(R.drawable.menu_icon)).perform(ViewActions.click());
+        onView(ViewMatchers.withContentDescription("Navigate up"))
+                .perform(ViewActions.click());
+
         onView(ViewMatchers.withId(R.id.nav_greeting)).perform(ViewActions.click());
 
-        onView(ViewMatchers.withId(R.id.fragmentContainerViewMain)).check(ViewAssertions.matches(ViewMatchers.withText("Greeting fragment")));
+        onView(ViewMatchers.withId(R.id.textGreeting))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
 
-        onView(ViewMatchers.withId(R.drawable.menu_icon)).perform(ViewActions.click());
-        onView(ViewMatchers.withId(R.id.nav_greeting)).perform(ViewActions.click());
+        onView(ViewMatchers.withContentDescription("Navigate up"))
+                .perform(ViewActions.click());
 
-        onView(ViewMatchers.withId(R.id.fragmentContainerViewMain)).check(ViewAssertions.matches(ViewMatchers.withText("Main fragment")));
+        onView(ViewMatchers.withId(R.id.nav_main)).perform(ViewActions.click());
+
+        onView(ViewMatchers.withId(R.id.textMain))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
         Intents.release();
     }
-
-
-    // --- Previous tests for the old structure of the MainActivity file; to be removed once approved
-    /*@Test
-    public void GoToGreetingActivityTest(){
-        Intents.init();
-        onView(ViewMatchers.withId(R.id.mainUserNameText)).perform(ViewActions.typeText("Roger"));
-        Espresso.closeSoftKeyboard();
-        onView(ViewMatchers.withId(R.id.mainGoButton)).perform(ViewActions.click());
-
-        Intents.intended(IntentMatchers.hasComponent(GreetingActivity.class.getName()));
-        Intents.release();
-
-    }
-
-    @Test
-    public void GoodGreetingMessageTest(){
-        onView(ViewMatchers.withId(R.id.mainUserNameText)).perform(ViewActions.typeText("Roger"));
-        Espresso.closeSoftKeyboard();
-        onView(ViewMatchers.withId(R.id.mainGoButton)).perform(ViewActions.click());
-
-        onView(ViewMatchers.withId(R.id.greetingText)).check(ViewAssertions.matches(ViewMatchers.withText("Bonjour Roger!")));
-    }*/
 }
