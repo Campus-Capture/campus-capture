88b96dcc2d004254f63968685a69198a255a7b3c
Mopplikus <elija-angelo.dirren@epfl.ch>
2023-03-24 15:24:02 +0100

    Implemented the updated current zone text, still needs to be tested due to DB issues.

--- app/src/main/java/com/github/campus_capture/bootcamp/fragments/MapsFragment.java
+++ app/src/main/java/com/github/campus_capture/bootcamp/fragments/MapsFragment.java
@@ -7,12 +7,15 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -24,19 +27,26 @@ import androidx.room.Room;
 import com.github.campus_capture.bootcamp.R;
 import com.github.campus_capture.bootcamp.storage.ZoneDatabase;
 import com.github.campus_capture.bootcamp.storage.dao.ZoneDAO;
+import com.github.campus_capture.bootcamp.storage.entities.Zone;
 import com.github.campus_capture.bootcamp.utils.PermissionUtils;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.maps.android.PolyUtil;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MapsFragment extends Fragment{
 
@@ -45,6 +55,9 @@ public class MapsFragment extends Fragment{
     private static final long HOUR_MILLIS = 36000000;
     private static final long MILLIS_PER_MIN = 60000;
     private static final long MILLIS_PER_SEC = 1000; // Yes, this is obvious as ship
+    private ZoneDatabase zoneDB;
+    private Timer currentZoneUpdate;
+    private static final long ZONE_REFRESH_RATE = 10 * MILLIS_PER_SEC; // The refresh rate of the current zone
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     /**
      * Flag indicating whether a requested permission has been denied
@@ -54,10 +67,11 @@ public class MapsFragment extends Fragment{
 
         //Build and initialize the DB
         //The DB contains the Zone around the campus
-        ZoneDatabase zoneDB = Room.databaseBuilder(getActivity(),
+        zoneDB = Room.databaseBuilder(getActivity(),
                 ZoneDatabase.class, "zones-db")
                 .createFromAsset("databases/zones-db.db")
                 .allowMainThreadQueries().build();
+        // zoneDB = Room.databaseBuilder(getActivity(), ZoneDatabase.class, "zones-db").build();
 
         ZoneDAO zoneDAO = zoneDB.zoneDAO();
 
@@ -79,9 +93,7 @@ public class MapsFragment extends Fragment{
         LatLng satellite = new LatLng(46.520544, 6.567825);
         map.addMarker(new MarkerOptions().position(satellite).title("Satellite").snippet("5 ⭐"));
 
-        /*LatLngBounds epflBounds = new LatLngBounds(
-                new LatLng(46, 6), // SW bounds
-                new LatLng(47, 7)  // NE bounds
+        /*
         );*/
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(epfl, 15));
 
@@ -95,6 +107,15 @@ public class MapsFragment extends Fragment{
             */
             return false;
         });
+
+        if(zoneDAO.findByName("campus") == null)
+        {
+            List<LatLng> epflBounds = new ArrayList<>();
+            epflBounds.add(new LatLng(46, 6)); // SW bounds
+            epflBounds.add(new LatLng(47, 7)); // NE bounds
+            Zone campusZone = new Zone("campus", epflBounds);
+            zoneDAO.insertAll(campusZone);
+        }
 
         Polygon campus = map.addPolygon(new PolygonOptions()
                 .clickable(true)
@@ -112,9 +133,6 @@ public class MapsFragment extends Fragment{
                     Toast.LENGTH_SHORT).show();
                     */
         });
-
-        //Don't forget to close when finished to be used
-        zoneDB.close();
     };
 
     private void enableMyLocation() {
@@ -198,16 +216,28 @@ public class MapsFragment extends Fragment{
                 + now.get(Calendar.SECOND) * MILLIS_PER_SEC
                 + now.get(Calendar.MILLISECOND));
         buttonTimer.start();
+
+        TextView zoneText = view.findViewById(R.id.currentZoneText);
+        startZoneTracking(zoneText);
     }
 
     @Override
     public void onDestroyView() {
         buttonTimer.cancel();
+        currentZoneUpdate.cancel();
+        zoneDB.close();
         super.onDestroyView();
     }
 
+    @Override
+    public void onDestroy()
+    {
+        zoneDB.close();
+        super.onDestroy();
+    }
+
     /**
-     * Method to create the timer on the wait button
+     * Method to create the timer on the wait button. Refreshes the timer every hour
      * @param button the button instance
      * @param hourDelta the time aready gone by since the last hour, in milliseconds
      */
@@ -232,6 +262,81 @@ public class MapsFragment extends Fragment{
                 buttonTimer.start();
             }
         };
+    }
+
+    /**
+     * Method whic finds in which zone a given point is
+     * @param position the position
+     * @return the (first) zone which matches the position in the DB, or null if none
+     */
+    private Zone findCurrentZone(LatLng position)
+    {
+        ZoneDAO zoneDAO = zoneDB.zoneDAO();
+        List<Zone> zones = zoneDAO.getAll();
+        for(Zone z : zones)
+        {
+            if(PolyUtil.containsLocation(position, z.getVertices(), false))
+            {
+                return z;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Method to get the current position of the player. Note that this uses a deprecated method
+     * of map, and should use a FusedLocationProviderClient instead
+     * @return LatLng or null
+     */
+    private LatLng getCurrentPosition()
+    {
+        try
+        {
+            if(!permissionDenied)
+            {
+                Location loc = map.getMyLocation();
+                return new LatLng(loc.getLatitude(), loc.getLongitude());
+            }
+        }
+        catch (Exception e)
+        {
+            Log.e("Exception: %s", e.getMessage(), e);
+        }
+        return null;
+    }
+
+    /**
+     * Method to start the zone tracking, and will update the current zone name in the text view
+     * @param text The textview where the zone name should be displayed
+     */
+    private void startZoneTracking(TextView text)
+    {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                String label = getString(R.string.current_zone_text);
+                LatLng position = getCurrentPosition();
+                if(position == null)
+                {
+                    label += "Unknown";
+                }
+                else
+                {
+                    Zone currentZone = findCurrentZone(position);
+                    if(currentZone == null)
+                    {
+                        label += "None";
+                    }
+                    else
+                    {
+                        label += currentZone.getName();
+                    }
+                }
+                text.setText(label);
+            }
+        };
+        currentZoneUpdate = new Timer();
+        currentZoneUpdate.scheduleAtFixedRate(task, 0, ZONE_REFRESH_RATE);
     }
 
 }
