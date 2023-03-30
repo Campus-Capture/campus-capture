efb1532cdb0b22597a6d8b8908777155fe2e0563
David Karoubi <david.karoubi@epfl.ch>
2023-03-30 18:22:35 +0200

    Tos Done

---
+++ app/src/main/java/com/github/campus_capture/bootcamp/authentication/TOS.java
@@ -0,0 +1,8 @@
+package com.github.campus_capture.bootcamp.authentication;
+
+public class TOS {
+    public static final String TEXT =
+        "By accepting those terms of service you agree that your data will be collected and used for non-legal activities such as DDoS-ing, Botting and...";
+
+    public static Boolean asAgreed;
+}
