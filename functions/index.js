b3b98933fd8e29052955eeb985734bccebc3882d
Raphael <raphael.temperli@epfl.ch>
2023-04-03 19:32:07 +0200

    first backend usable

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,11 @@
 const functions = require("firebase-functions");
 
+// Import Admin SDK
+const { getDatabase } = require('firebase-admin/database');
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
@@ -9,11 +15,57 @@ const functions = require("firebase-functions");
 //  });
 
 // the schedule is specified in cron job format "15 * * * *" = "At minute 15."
-exports.scheduledFunction = functions.region('europe-west1').pubsub.schedule("15 * * * *").onRun((context) => {
+
+
+
+const NUMBER_OF_ZONES = 2
+
+
+exports.testScheduledFunction = functions.region('europe-west1').pubsub.schedule("* * * * *").onRun((context) => {
+
+    console.log("It's minute 15!");
   
-  //functions.logger.info("It's minute 15!");
-  console.log("It's minute 15!");
+    const db = getDatabase();
+    const ref = db.ref('Zones');
+
+    //snap.numChildren()
 
 
-  return null;
+    ref.once('value', (snapshot) => {
+
+        for( let i = 1; i <= NUMBER_OF_ZONES; i++){
+
+            var owner = "no owner"
+            var max = 0
+
+            let val_IN = snapshot.child(i).child("IN").val()
+            console.log("IN : " + val_IN)
+            if(val_IN > max){
+                console.log("val > max")
+                max = val_IN
+                owner = "IN"
+            }
+
+            let val_SC = snapshot.child(i).child("SC").val()
+            console.log("SC : " + val_SC)
+            if(val_SC > max){
+                console.log("val > max")
+                max = val_SC
+                owner = "SC"
+            }
+
+            console.log("new owner is " + owner)
+
+            ref.child(i).child("owner").set(owner, (error) => {
+                if (error) {
+                  console.log('Data could not be saved.' + error);
+                } else {
+                  console.log('Data saved successfully.');
+                }
+            });
+        }
+
+    });
+
+    return null;
 });
