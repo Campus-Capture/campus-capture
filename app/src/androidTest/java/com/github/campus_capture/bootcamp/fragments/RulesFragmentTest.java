329d11f285dce328489160e03d0ad620d7aa5f5c
Mopplikus <elija-angelo.dirren@epfl.ch>
2023-04-05 20:39:07 +0200

    Added a test for the rules fragment

---
+++ app/src/androidTest/java/com/github/campus_capture/bootcamp/fragments/RulesFragmentTest.java
@@ -0,0 +1,42 @@
+package com.github.campus_capture.bootcamp.fragments;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.containsString;
+
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.github.campus_capture.bootcamp.R;
+import com.github.campus_capture.bootcamp.activities.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class RulesFragmentTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> testRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void checkThatRulesAreCorrectlyDisplayed()
+    {
+        Intents.init();
+
+        onView(ViewMatchers.withContentDescription("Navigate up"))
+                .perform(ViewActions.click());
+
+        onView(ViewMatchers.withId(R.id.nav_rules)).perform(ViewActions.click());
+
+        onView(ViewMatchers.withId(R.id.rules_text)).check(matches(withText(containsString("Welcome to"))));
+
+        Intents.release();
+    }
+
+}
