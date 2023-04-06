5ecae0933545bf019beb1b61a44e0f49d11a2d3a
Raphael <raphael.temperli@epfl.ch>
2023-04-06 17:51:19 +0200

    very approx tests refactor

--- app/src/test/java/com/github/campus_capture/bootcamp/PlaceholderBackendInterfaceTest.java
+++ app/src/test/java/com/github/campus_capture/bootcamp/PlaceholderBackendInterfaceTest.java
@@ -19,34 +19,60 @@ public class PlaceholderBackendInterfaceTest {
     public void testVoteZoneReturnsCorrect()
     {
         PlaceholderFirebaseInterface t = new PlaceholderFirebaseInterface();
-        assertTrue(t.voteZone("", Section.IN, ""));
+
+        // TODO better way ?
+        try {
+            assertTrue(t.voteZone("", Section.IN, "").get());
+        }catch(Exception e){
+            assertTrue(false);
+        }
     }
 
     @Test
     public void testCurrentZoneOwners()
     {
         PlaceholderFirebaseInterface t = new PlaceholderFirebaseInterface();
-        Map<String, Section> owners = t.getCurrentZoneOwners();
-        assertEquals(owners.get("campus"), Section.IN);
+
+        // TODO better way ?
+        try{
+            Map<String, Section> owners = t.getCurrentZoneOwners().get();
+            assertEquals(owners.get("campus"), Section.IN);
+        }catch(Exception e){
+            assertTrue(false);
+        }
     }
 
     @Test
     public void testScoresAreWellOrdered()
     {
         PlaceholderFirebaseInterface t = new PlaceholderFirebaseInterface();
-        List<ScoreItem> scores = t.getScores();
 
-        for(int i = 0; i < scores.size() - 1; i++)
-        {
-            assertTrue(scores.get(i).getValue() >= scores.get(i + 1).getValue());
+        // TODO better way ?
+        try{
+            List<ScoreItem> scores = t.getScores().get();
+
+            for(int i = 0; i < scores.size() - 1; i++)
+            {
+                assertTrue(scores.get(i).getValue() >= scores.get(i + 1).getValue());
+            }
+        }catch(Exception e){
+            assertTrue(false);
         }
+
     }
 
     @Test
     public void testIfPlayerAlreadyAttacked()
     {
         PlaceholderFirebaseInterface t = new PlaceholderFirebaseInterface();
-        assertFalse(t.hasAttacked("kek"));
+
+        // TODO better way ?
+        try{
+            assertFalse(t.hasAttacked("kek").get());
+        }catch(Exception e){
+            assertTrue(false);
+        }
+
     }
 
 }
