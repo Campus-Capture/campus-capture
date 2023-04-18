1e2caee23371d84afbe35190641264fc7e4f647d
Mopplikus <elija-angelo.dirren@epfl.ch>
2023-04-18 12:08:35 +0200

    Added the sharing mechanism

--- app/src/main/java/com/github/campus_capture/bootcamp/fragments/ProfileFragment.java
+++ app/src/main/java/com/github/campus_capture/bootcamp/fragments/ProfileFragment.java
@@ -1,14 +1,18 @@
 package com.github.campus_capture.bootcamp.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
 
 import com.github.campus_capture.bootcamp.R;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +20,17 @@ import com.github.campus_capture.bootcamp.R;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+
+    private final View.OnClickListener invite_listener = v -> {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        String message = getString(R.string.invitation_text) + getString(R.string.invitation_link);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, message);
+        sendIntent.setType("text/plain");
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        startActivity(shareIntent);
+    };
+
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -46,6 +61,11 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button invite_button = view.findViewById(R.id.invite_button);
+        invite_button.setOnClickListener(invite_listener);
+
+        return view;
     }
 }
