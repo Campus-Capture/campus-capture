2063b73fef4f345f7309b947b4437714fa33a263
Antoine Moix <antoine.moix@epfl.ch>
2023-05-18 17:27:18 +0200

    Improved coverage on PermissionsUtils
    I hope that the CI will not get angry

--- app/src/androidTest/java/com/github/campus_capture/bootcamp/utils/PermissionsUtilsTest.java
+++ app/src/androidTest/java/com/github/campus_capture/bootcamp/utils/PermissionsUtilsTest.java
@@ -9,8 +9,13 @@ import static org.junit.Assert.fail;
 import android.Manifest;
 import android.app.Instrumentation;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.RemoteException;
+import android.provider.Settings;
 import android.util.Log;
+import android.widget.RadioButton;
 
 import androidx.core.content.ContextCompat;
 import androidx.room.Room;
@@ -38,12 +43,51 @@ import java.util.stream.IntStream;
 
 @RunWith(AndroidJUnit4.class)
 public class PermissionsUtilsTest {
+
     @Before
-    public void removePermissions(){
-        InstrumentationRegistry.getInstrumentation().getUiAutomation().executeShellCommand("pm revoke com.github.campus_capture.bootcamp android.permission.ACCESS_FINE_LOCATION");
-        InstrumentationRegistry.getInstrumentation().getUiAutomation().executeShellCommand("pm revoke com.github.campus_capture.bootcamp android.permission.ACCESS_COARSE_LOCATION");
-        //InstrumentationRegistry.getInstrumentation().getUiAutomation().revokeRuntimePermission("com.github.campus_capture.bootcamp", "android.permission.ACCESS_COARSE_LOCATION");
-        //InstrumentationRegistry.getInstrumentation().getUiAutomation().revokeRuntimePermission("com.github.campus_capture.bootcamp", "android.permission.ACCESS_FINE_LOCATION");
+    public void removePermissions() throws IOException, InterruptedException {
+        final Intent i = new Intent();
+        i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        i.addCategory(Intent.CATEGORY_DEFAULT);
+        i.setData(Uri.parse("package:" + getTargetContext().getApplicationContext().getPackageName()));
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+        getTargetContext().getApplicationContext().startActivity(i);
+
+        Instrumentation ir = InstrumentationRegistry.getInstrumentation();
+        UiObject perm = UiDevice.getInstance(ir).findObject(new UiSelector().textContains("Permissions"));
+
+        try {
+            perm.click();
+        } catch (UiObjectNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        Thread.sleep(2000);
+
+        UiObject loc = UiDevice.getInstance(ir).findObject(new UiSelector().textContains("Location"));
+        try {
+            loc.click();
+        } catch (UiObjectNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        Thread.sleep(2000);
+        UiObject allow = UiDevice.getInstance(ir).findObject(new UiSelector().className(RadioButton.class.getName()).index(2));
+        try {
+            allow.click();
+        } catch (UiObjectNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @After
+    public void closeSettingsApp() throws IOException, RemoteException {
+        Instrumentation ir = InstrumentationRegistry.getInstrumentation();
+        UiDevice uiD = UiDevice.getInstance(ir);
+        for(int i = 0; i < 10; i++){
+            uiD.pressBack();
+        }
     }
 
     public void grantPermission(){
@@ -59,14 +103,26 @@ public class PermissionsUtilsTest {
         }
     }
 
+    public void denyPermission(){
+        Instrumentation ir = InstrumentationRegistry.getInstrumentation();
+        UiObject uo = UiDevice.getInstance(ir).findObject(new UiSelector().textContains("Don't allow"));
+
+        if(uo.exists()){
+            try {
+                uo.click();
+            } catch (UiObjectNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
     @Test
     public void testRequestLocationPermissionGrant() throws Exception {
         ActivityScenario.launch(MainActivity.class);
         if (ContextCompat.checkSelfPermission(getTargetContext().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
-            fail("Test should not have access to location. Has the @Before ran?");
+            fail("App should not have location permission. Has @Before ran?");
         }
-
         Thread.sleep(2000);
         //app has no access to permission, which is excepted
         grantPermission();
@@ -77,14 +133,26 @@ public class PermissionsUtilsTest {
             return;
         }
 
-        fail("nique toi");
+        fail("Something went wrong, permission was not granted");
     }
 
     @Test
     public void testRequestLocationPermissionDeny() throws Exception {
-        //check that no perm
-        //request permission
-        //deny
-        //check that perm is not granted
+        ActivityScenario.launch(MainActivity.class);
+        if (ContextCompat.checkSelfPermission(getTargetContext().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            fail("App should not have location permission. Has @Before ran?");
+        }
+        Thread.sleep(2000);
+        //app has no access to permission, which is excepted
+        denyPermission();
+        Thread.sleep(2000);
+
+        if (ContextCompat.checkSelfPermission(getTargetContext().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+
+        fail("Something went wrong, permission was granted");
     }
 }
