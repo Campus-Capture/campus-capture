532aec1357b7d92f293f3f823f110a1e778f11ec
Antoine Moix <antoine.moix@epfl.ch>
2023-05-19 09:06:59 +0200

    Update ScheduleConstants.java

--- app/src/main/java/com/github/campus_capture/bootcamp/map/ScheduleConstants.java
+++ app/src/main/java/com/github/campus_capture/bootcamp/map/ScheduleConstants.java
@@ -7,7 +7,7 @@ public class ScheduleConstants {
     public static final long MILLIS_PER_HOUR = 60 * MILLIS_PER_MIN;
     public static final long ZONE_REFRESH_RATE = 10 * MILLIS_PER_SEC;
     public static final long OWNER_REFRESH_DELAY = 5 * MILLIS_PER_SEC;
-    public static final long TAKEOVER_DURATION = 15 * MILLIS_PER_MIN;
+    public static final long TAKEOVER_DURATION = 35 * MILLIS_PER_MIN;
     public static final long NO_POSITION_RETRY_DELAY = MILLIS_PER_SEC;
 
 }
